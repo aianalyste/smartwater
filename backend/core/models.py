eb979d02d28e38b47706bf0894f451c3bd0cadbc
aianalyste <blanckombate93@gmail.com>
2026-08-22 00:34:32 +0000

    Ajoute is_authenticated pour compatibilite DRF

--- backend/core/models.py
+++ backend/core/models.py
@@ -41,7 +41,7 @@ class Utilisateur(models.Model):
     session_token = models.CharField(max_length=64, unique=True, editable=False)
     date_creation = models.DateTimeField(auto_now_add=True)
 
-        def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         if not self.session_token:
             import secrets
             self.session_token = secrets.token_urlsafe(32)
