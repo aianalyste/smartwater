65f24633f5a46881f002260dd450ef74247b0acf
aianalyste <blanckombate93@gmail.com>
2026-08-27 22:27:11 +0000

    Recompilation avec automatisation

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3844797744" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "974898861" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
